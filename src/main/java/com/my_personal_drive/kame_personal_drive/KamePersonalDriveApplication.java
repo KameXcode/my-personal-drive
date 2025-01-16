142ac5252c5796ce11f5b47f1dfa5a946b5f6a30
unknown <kauadesousafelicio06@gmail.com>
2025-01-15 22:19:59 -0300

    creating the application

---
+++ src/main/java/com/my_personal_drive/kame_personal_drive/KamePersonalDriveApplication.java
@@ -0,0 +1,13 @@
+package com.my_personal_drive.kame_personal_drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamePersonalDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamePersonalDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my_personal_drive/kame_personal_drive/KamePersonalDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_personal_drive.kame_personal_drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamePersonalDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
